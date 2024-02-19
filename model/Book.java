02795f2f87f1f58bafe8b30eb9e4235db1df7c49
Perry512 <perry512@hotmail.com>
2024-02-19 16:24:06 -0500

    [ADD] Properties: Added base methods to Book object

--- model/Book.java
+++ model/Book.java
@@ -82,6 +82,128 @@ public abstract class Book extends EntityBase implements IView {
         // setDependencies();
         persistentState = new Properties();
         Enumeration allKeys = props.propertyNames();
-        
+        while (allKeys.hasMoreElements()) {
+
+            String nextKey = (String)allKeys.nextElement();
+            String nextValue = props.getProperty(nextKey);
+
+            if (nextValue != null) {
+
+                persistentState.setProperty(nextKey, nextValue);
+
+            }
+        }
+
+    }
+
+    private void setDependencies() {
+
+        dependencies = new Properties();
+        myRegistry.setDependencies(dependencies);
+
+    }
+
+    public Object getState(String key) {
+
+        if (key.equals("UpdateStatusMessage")) {
+
+            return updateStatusMessage;
+        }
+
+        return persistentState.getProperty(key);
+
+    }
+
+    public void stateChangeRequest(String key, Object value) {
+
+        myRegistry.updateSubscribers(key, this);
+
+    }
+
+    public void updateState(String key, Object value) {
+
+        stateChangeRequest(key, value);
+
+    }
+
+    // public boolean verifyOwnership(AccountHolder cust) {
+
+    //     if (cust == null) {
+
+    //         return false;
+
+    //     } else {
+
+    //         String custid = (String)cust.getState("ID");
+    //         String myOwnerid = (String)getState("OwnerID");
+
+    //         return (custid.equals(myOwnerid));
+
+    //     }
+
+    // }
+
+    public void update() {
+
+        updateStateInDatabase();
+
+    }
+
+    private void updateStateInDatabase() {
+
+        try {
+
+            if (persistentState.getProperty("bookId") != null) {
+
+                Properties whereClause = new Properties();
+
+                whereClause.setProperty("bookId",
+                persistentState.getProperty("bookId"));
+
+                updatePersistentState(mySchema, persistentState, whereClause);
+
+                updateStatusMessage = "Book data for book number: " + persistentState.getProperty("bookId") + "updated successfully";
+
+            } else {
+
+                Integer bookNumber =  
+                    insertAutoIncrementalPersistentState(mySchema, persistentState);
+                
+                    persistentState.setProperty("bookId", "" + bookNumber.intValue());
+
+                    updateStatusMessage = "Book data for new book: " + persistentState.getProperty("bookId")
+                        + "successfully added to database!";
+
+            }
+
+        } catch (SQLException e) {
+
+            updateStatusMessage = "Error in installing book data: " + e + "\n";
+
+        }
+
+    }
+
+    public Vector<String> getEntryListView() {
+
+        Vector<String> v = new Vector<String>();
+
+        v.addElement(persistentState.getProperty("bookId"));
+        v.addElement(persistentState.getProperty("bookTitle"));
+        v.addElement(persistentState.getProperty("author"));
+        v.addElement(persistentState.getProperty("pubYear"));
+        v.addElement(persistentState.getProperty("status"));
+
+        return v;
+
+    }
+
+    protected void initializeSchema(String tableName) {
+
+        if(mySchema == null) {
+
+            mySchema = getSchemaInfo(tableName);
+            
+        }
     }
 }
